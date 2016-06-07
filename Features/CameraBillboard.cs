78ccd93dc1cd3cffd85249939d2519800377afce
Brandon <lrbn@users.noreply.github.com>
2016-06-06 23:40:55 -0400

    Create CameraBillboard.cs

---
+++ Features/CameraBillboard.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraBillboard : MonoBehaviour
+{
+	public Camera m_Camera;
+
+	void Update()
+	{
+		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+			m_Camera.transform.rotation * Vector3.up);
+	}
+}
+
